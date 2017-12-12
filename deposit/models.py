364efcec9b3b40d218f6c4102d00be76e724808f
pyc0d333 <barjebernard@gmail.com>
2017-12-12 23:29:15 +0000

    deposit model created

---
+++ deposit/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class  Deposit(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='user_deposits', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00 )
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
