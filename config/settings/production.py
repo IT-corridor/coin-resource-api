d72dbf569ac231ff3e2274c0106088f5e1785bc8
pyc0d333 <barjebernard@gmail.com>
2017-12-24 15:50:55 +0000

    allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -48,7 +48,7 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['mutualcoin.fund','api.mutualcoin.fund', '137.135.96.39', '*']
+ALLOWED_HOSTS = ['*']
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ['gunicorn', ]
