d548908c0bee6df824136d61f05dc78c8fbcf363
pyc0d333 <barjebernard@gmail.com>
2018-01-13 21:04:05 +0000

    new field for sell

--- assets/models.py
+++ assets/models.py
@@ -32,7 +32,7 @@ class SoldAssets(models.Model):
     symbol = models.CharField(max_length=200, default='')
     image_url = models.CharField(max_length=200, default='')
     asset_id = models.CharField(max_length=2, default='')
-    purchase_price = models.DecimalField(max_digits=32, default=0.00, decimal_places=12)
+    purchase_price = models.DecimalField(max_digits=32, default=0.00, decimal_places=8)
     amount = models.DecimalField(max_digits=32, decimal_places=8, default=0.00)
     sell_price = models.DecimalField(max_digits=20, decimal_places=12, default=0.00)
     buy_price = models.DecimalField(max_digits=20, decimal_places=12, default=0.00)
